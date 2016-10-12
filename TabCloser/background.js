17491d5442713fbef8a4bf03bee4106f906daf5e
Roshan Dash <roshandash411@gmail.com>
2016-10-12 12:38:05 +0530

    Update background.js

--- TabCloser/background.js
+++ TabCloser/background.js
@@ -1,7 +1,8 @@
+//The Browser Action present on the Toobar.
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.remove(tab.id);
 });
-
+//Creates the Right Click Context Menu and adds option to Close it!
 chrome.contextMenus.create({"title": "TabCloser"});
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
   chrome.tabs.remove(tab.id);
